1c4435da5ba7b6fa930fc6d5b2dd0484fbee5c68
alexandrogong <766842290@qq.com>
2018-12-29 13:23:58 +0800

    发布抓取teradyne TUG paper代码，version1.0

--- alexandrocode/tugpaperDownload.py
+++ alexandrocode/tugpaperDownload.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -21,68 +22,130 @@ def get_link(content):
 
 
 if __name__ == "__main__":
-    login_id = "****"
-    password = "****"
-    path = r"C:\\Users\\gonghaiq\\Desktop\\Tug paper\\"
+
+    # # 获取cookies 保存到cookies文件夹。
+    # driver = webdriver.Chrome()
+    # driver.get(m_url)
+    # d_cookies = driver.get_cookies()
+    # json_cookies = json.dumps(d_cookies)
+    # with open('.\cookies\eknowledge_cookies.txt', 'w') as f:
+    #     f.write(json_cookies)
+    # driver.close()
+    # driver.quit()
+
+
+    # 打开 Teradyne TUG主页
+    m_path = r"C:\\Users\\gonghaiq\\Desktop\\Tug paper\\"
+
     m_url = r"https://eknowledge.teradyne.com/wps/myportal/eknowledge/my/semiconductor_test" \
               r"/stdglobalsearch/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8zi3Q0M3D0s_A183J2dnQ0cP" \
               r"SwtTAIMggwNjIz0w1EVGJiGGho4hrqa-buHOhm4e5voR5Gk398lwMXA0TUoxNHRzNXQN9SIOP0GOICjAYn" \
               r"2YyqIwm98uH4UqhVYQgBVARYvErKkIDc0NDTCINPTUVERABwYwbc!/dz/d5/L2dBISEvZ0FBIS9nQSE" \
               r"h/?searchGlobal_scope=TUGSecCollection&SortBy=date"
 
-    c_cookies = r"__utmz=71283093.1513142159.5.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic;" \
-                r" hubspotutk=ea66bb46e05b359f4edd43fbc52fb76b; __hs_opt_out=no; __hstc=267904273" \
-                r".ea66bb46e05b359f4edd43fbc52fb76b.1512720983557.1542766727749.1542853357073.49; " \
-                r"__utma=71283093.1786325429.1512720983.1545725175.1545794471.55; __utmc=71283093; __" \
-                r"hssrc=1; __unam=eca3cfc-160353264b1-20968ad5-259; LtpaToken2=0ZmdYdYh7qUUhk5xu/cWI+C" \
-                r"lqyvmK51hhBDCVAgtnrEhjKV7/dBGMyXL+EgGbQpP1ZO6NcF4BCuCqCkk7jVq/jsjzohgamoSQIxylGABNCCa+" \
-                r"8hwDLKEzHdpX6eMXBzKXJXv52GycnTKqS/lWclAyOv6eg5cZYGb0IstOzs+8nCnxF0pm8e0N00O0tJBYFQiQFEU" \
-                r"HHt1SvZXrIy7BdELoEttJ8bVU9Qek/bq/IM9Zka+AiCC3IOOwn1W9CmuJMKNhLpzyS2vboehTHnylsIDduJMKngmr" \
-                r"6F3qn0ar3j4NFmEUf7I055d1mjAtfwSltUZDuex+Nbq3Xdr7x+Orm/AaGTw9cNicvTaH8x13NknbCWAxHtquR9mgEi0jF" \
-                r"t3ZlCs; portal=1293444483.1.1409054128.3707520480; JSESSIONID=0000hxnZz4hKsr6GG2t9gLAvUFU:19vb00" \
-                r"odv; PD-H-SESSION-ID=1_aytbbBOYTOOsdR6lYNzNpi8Y++UVOgbCU4sQasyPgIIrQO3RfrQ=_AAAAAwA=_oX38BIIxdWk" \
-                r"DW99tYuhl/FN1RCI=; _pk_id.11.3ab8=1835ce7f460dd574.1512721124.76.1545884671.1545883624."
+    # load cookies
+    with open('.\cookies\eknowledge_cookies.txt', 'r') as f:
+        list_cookies = json.loads(f.read())
 
-    d_cookies ={}
-    for line in c_cookies.split(";"):
-        key, value = line.split("=", 1)
-        d_cookies[key] = value
+    # 打开teradyne TUG 主页=====================================================================
+    # 设置选项 不弹窗，下载默认地址为指定地址 禁用pdf viewer
+    options = webdriver.ChromeOptions()
+    prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': 'C:\\Users\\gonghaiq\\Desktop\\Tug paper\\TUG_PY',
+             "profile.default_content_settings.pdf_documents": 1}
+    options.add_experimental_option('prefs', prefs)
+    driver = webdriver.Chrome(executable_path='C:\\Users\\gonghaiq\\AppData\\Local\\Programs\\Python\\Python36-32\\chromedriver.exe', chrome_options=options)
 
-    d_headers = {'User-agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                               "Chrome/71.0.3578.98 Safari/537.36"}
-
-    # 打开 Teradyne TUG主页
-    driver = webdriver.Chrome()
+    driver.get("https://eknowledge.teradyne.com")
+    for cookie in list_cookies:
+        driver.add_cookie({'name': cookie['name'],
+                           'value': cookie['value'],
+                           'expires': None})
     driver.get(m_url)
-    time.sleep(1)
+    time.sleep(5)
 
     # 选择ultraFlexFamily
     ultraFlexFamily = driver.find_element_by_xpath("//input[@value='Product Type$UltraFLEX Family']")
     ultraFlexFamily.click()
-    time.sleep(1)
+    time.sleep(5)
 
-    # 获取每页link
-    html_text = driver.page_source
-    m_links = get_links(html_text)
 
-    # 下载每一个Link中的TUG paper
+    # 获取每页link并将link全部要写入文件==================================================================================
+    # html_text = driver.page_source
+    # m_links = get_links(html_text)
+
+    # with open('.\TUG\paper_links.txt', 'w') as f:
+    #     # 写入第一次的30个links
+    #     for link in m_links:
+    #         f.write(link+'\n')
+    #     # 点击下一页, 获取下一页的内容(total 80 pages)
+    #     i = 0
+    #     for i in range(80):
+    #         next_page = driver.find_element_by_xpath("//a[@onclick='nextRecords()']")
+    #         next_page.click()
+    #         time.sleep(1)
+    #         html_text = driver.page_source
+    #         m_links = get_links(html_text)
+    #         for link in m_links:
+    #             f.write(link + '\n')
+    #         time.sleep(5)
+    #         print("成功下载" + str(i) + "次")
+    #         i += 1
+
+
+    # 下载文件中的所有链接===============================================================================================
     i = 0
-    for element in m_links:
-        link = element.split("\"")[1]
-        driver.get(link)
-        download = driver.find_element_by_xpath("//div[@class='col-sm-2']/a[@class='btn btn-primary wcm-download-button-width']")
-        download.click()
-        time.sleep(10)
-        print("成功下载" + str(i))
-        i += 1
+    with open('.\TUG\paper_links.txt', 'r') as f, open('.\TUG\paper_index.txt', 'w') as f1:
+        link = f.readline()
+        while link:
+            # 重新给cookie, 打开网页
+            # driver.get(m_url)
+            for cookie in list_cookies:
+                driver.add_cookie({'name': cookie['name'],
+                                   'value': cookie['value'],
+                                   'expires': None})
+            driver.get(link.split("\"")[1])
+            download = driver.find_element_by_xpath("//div[@class='col-sm-2']/a[@class='btn btn-primary wcm-download-button-width']")
+            download.click()
+            pdf_name = driver.find_element_by_xpath("//div[@class='col-sm-10 padding-filename-text']/span[@id='wcmFile1Name']").text
+            paper_name = driver.find_element_by_xpath("//div[@class='panel-heading']/h3[@class='panel-title']").text
 
-    # 点击下一页, 获取下一页的内容(total 80 pages)
-    # for i in range(2):
-    #     next_page = driver.find_element_by_xpath("//a[@onclick='nextRecords()']")
-    #     next_page.click()
-    #     time.sleep(1)
-    #     html_text = driver.page_source
-    #     m_links.extend(get_links(html_text))
+            time.sleep(10)
+            link = f.readline()
+
+            f1.write(paper_name + "_" + pdf_name + "\n")
+
+            print("成功下载" + str(i))
+            i += 1
+
+    print("finish download...")
+
+
+
+
+
+
+
+
+
+    # # 下载每一个Link中的TUG paper
+    # i = 0
+    # for element in m_links:
+    #     link = element.split("\"")[1]
+    #
+    #     # driver.get(link)
+    #     for cookie in list_cookies:
+    #         driver.add_cookie({'name': cookie['name'],
+    #                            'value': cookie['value'],
+    #                            'expires': None})
+    #
+    #     driver.get(link)
+    #     download = driver.find_element_by_xpath("//div[@class='col-sm-2']/a[@class='btn btn-primary wcm-download-button-width']")
+    #     download.click()
+    #     time.sleep(10)
+    #     print("成功下载" + str(i))
+    #     i += 1
+
+
 
 
 
